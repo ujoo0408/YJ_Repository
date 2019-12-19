287632ac29aa4694a20ff995a93653137709dd58
ujoo0408 <49928214+ujoo0408@users.noreply.github.com>
2019-12-20 01:35:41 +0900

    191219 파이썬&루비 기본 예제

---
+++ Python_Ruby/inOutput.rb
@@ -0,0 +1,13 @@
+# 입력과출력
+
+puts("아이디를 입력해주세요")
+input = gets.chomp()
+real_YJ = "ujoo0408"
+real_ujoo0408 = "YJ"
+if real_YJ == input
+  puts("Hello!, ujoo")
+elsif real_ujoo0408 == input
+  puts("Hello!, ujoo0408")
+else
+  puts("Who are you?")
+end
---
+++ Python_Ruby/variable.rb
@@ -0,0 +1,26 @@
+# 변수
+
+x=10
+y=5
+puts(x+y)		# 15
+ 
+title = "python & ruby"
+puts("Title is "+title)		# Title is python & ruby
+
+
+
+# 문자열에서 변수의 사용
+
+name = "박유주"
+puts("안녕하세요. "+name+"님")				# 안녕하세요. 박유주님
+puts(name+"님을 위한 강의를 준비했습니다.")		# 박유주님을 위한 강의를 준비했습니다.
+puts(name+"님 꼭 참석 부탁드립니다.")			# 박유주님 꼭 참석 부탁드립니다.
+
+
+
+#수 계산에서 변수의 사용
+
+donation = 200
+student = 10
+sponsor = 100
+puts((donation*student)/sponsor) #20
