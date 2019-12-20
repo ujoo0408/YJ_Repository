68a133b379f8510654e51e2885b00417051a9aa6
YJ <ujoo0408@naver.com>
2019-12-21 01:56:48 +0900

    191220 자바 메소드 예제

---
+++ projectMethod/src/projectMethod/OutputMethod.java
@@ -0,0 +1,21 @@
+package projectMethod;
+
+public class OutputMethod {
+	public static String a() {
+		//...
+		
+		return "a";
+	}
+	
+	public static int one() {
+		return 1;
+	}
+	
+	//void는 리턴값 없음
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(a());
+		System.out.println(one());
+	}
+
+}
